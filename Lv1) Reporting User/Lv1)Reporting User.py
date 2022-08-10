1843bb892bdef1e9c33a0ee5c4f558c0b894cad6
roso275 <yongrok0527@gmail.com>
2022-08-11 04:43:59 +0900

    solved

--- Lv1) Reporting User/Lv1)Reporting User.py
+++ Lv1) Reporting User/Lv1)Reporting User.py
@@ -1,13 +1,10 @@
 def solution(id_list, report, k):
-    answer = []
+    answer = [0 for i in id_list]
     report_set = set(report)
-
     report_contents = []
     report_contents_2 =[]
     sorted_report = []
     report_count = []
-    user_report_id = []
-
 
     for i in report_set:
         temp = i.split()
@@ -21,39 +18,24 @@ def solution(id_list, report, k):
     for i in id_list:
         report_count.append(report_contents_2.count(i))
 
-    print(sorted_report)
     
     temp_user_report_id = []
-
     for i in sorted_report:
         if temp_user == i[0]:
             temp_user_report_id.append(i[1])
-            print(i[1])
-        else:
-            temp_user == i[0]
-            print("else : ", i)
-            user_report_id.append(temp_user_report_id)
-            print("report id : " , user_report_id)
-            temp_user_report_id = []  # .clear() , = [] 차이 ??
-            print(temp_user_report_id)
+        else:            
+            for j in temp_user_report_id: 
+                if report_count[id_list.index(j)]>= k:               
+                    answer[id_list.index(temp_user)] += 1
+
+            temp_user = i[0]
+            temp_user_report_id = [] 
             temp_user_report_id.append(i[1])
-            print(temp_user_report_id)
-
-    print (user_report_id)
-    print (report_count)
-
-    #solting and Create user_report_id List  
     
+    for j in temp_user_report_id: 
+        if report_count[id_list.index(j)]>= k:               
+            answer[id_list.index(temp_user)] += 1
 
-        
-    for j in user_report_id:
-
-        count = 0
-        for m in j:            
-            if report_count[id_list.index(m)]>= k:                
-                count += 1
-        answer.append(count)
-        
     return answer
 
 id_list = ["muzi", "frodo", "apeach", "neo"]
